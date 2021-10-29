e9629707470507dae95276c36fd4b8d61a2de6e5
Yana <belozub.yanochka@mail.ru>
2021-10-29 22:42:34 +0400

    split a common file into multiple files

---
+++ Task-06_JS-Basics/6_4_accumulate.js
@@ -0,0 +1,11 @@
+console.log("Задание 6.4. – Форматирование строки");
+
+function accumulate(str) {
+    return str
+        .split('')
+        .reduce((str, letter, index) => str.length ? str + '-' + letter.toUpperCase() + letter.toLowerCase().repeat(index) : letter.toUpperCase(), '');
+};
+
+console.log(accumulate('abcd'));
+console.log(accumulate('RqaEzty'));
+console.log(accumulate('cwAt'));
