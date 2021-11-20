904124f9ab2d101f31c43bd3b5e3b3973a57ccba
Yana <belozub.yanochka@mail.ru>
2021-11-20 17:44:20 +0400

    functions added

--- Task-08_JS_Functions/8_1_generatePassword.js
+++ Task-08_JS_Functions/8_1_generatePassword.js
@@ -1,5 +1,31 @@
-function generatePassword(){
-    
+function generatePassword() {
+  let array = [];
+  let numberChars = "0123456789";
+  let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowerChars = "abcdefghiklmnopqrstuvwxyz";
+
+  let passwordLength = randomInteger(6, 20);
+  let password = arrayRandElement(numberChars) + arrayRandElement(upperChars) + arrayRandElement(lowerChars);
+
+  array = array.concat(numberChars.split(""))
+    .concat(upperChars.split(""))
+    .concat(lowerChars.split(""))
+
+  while (password.length < passwordLength) {
+    password += array[randomInteger(0, array.length - 1)];
   }
-  console.log("Password generator: ")
-  console.log("");
+  return password;
+}
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+function arrayRandElement(arr) {
+  let rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+}
+
+console.log("Password generator: ")
+console.log(generatePassword())
+console.log("");
--- Task-08_JS_Functions/8_2_eleminationGame.js
+++ Task-08_JS_Functions/8_2_eleminationGame.js
@@ -1,6 +1,19 @@
-function eleminate() {
+function eleminate(quantity, step) {
+    let participantList = new Array(quantity).fill().map((e, i) => i + 1)
 
+    for (let i = 1; participantList.length > 1; i++) {
+        let current = participantList.shift();
+        !(i % step == 0)
+            ? participantList.push(current)
+            : i = 0;
+    }
+    return participantList.shift();
 }
 
-console.log("Elemination game: ")
+console.log("Elemination game: ");
+console.log(eleminate(7, 3));
+console.log(eleminate(11, 19));
+console.log(eleminate(1, 300));
+console.log(eleminate(14, 2));
+console.log(eleminate(100, 1));
 console.log("");
