bd54629ce11e41da52959387aedd6b9cb3dd0640
Yana <belozub.yanochka@mail.ru>
2021-11-27 17:51:53 +0400

    styles added

--- Task-09_JS_and_DOM/index.js
+++ Task-09_JS_and_DOM/index.js
@@ -10,25 +10,37 @@ var DEGREE_INPUT = document.getElementById("degree-input");
 var ELEMINATE_INPUT_N = document.getElementById("eleminate-input-N");
 var ELEMINATE_INPUT_M = document.getElementById("eleminate-input-M");
 
-document.querySelector(".binary-clearButton").onclick = () => {
+document.querySelector(".binary-clearButton").onclick = (event) => {
+    event.preventDefault();
     BINARY_INPUT.value = "";
     BINARY_RESULT.textContent = " ";
 }
 
-document.querySelector(".degree-clearButton").onclick = () => {
+document.querySelector(".degree-clearButton").onclick = (event) => {
+    event.preventDefault();
     DEGREE_INPUT.value = "";
     DEGREE_RESULT.textContent = " ";
 }
 
-document.querySelector(".eleminate-clearButton").onclick = () => {
+document.querySelector(".eleminate-clearButton").onclick = (event) => {
+    event.preventDefault();
     ELEMINATE_INPUT_N.value = "";
     ELEMINATE_INPUT_M.value = "";
     ELEMINATE_RESULT.textContent = " ";
 }
 
-document.querySelector(".binary-button").onclick = () => BINARY_RESULT.textContent = makeBinary(BINARY_INPUT.value);
+document.querySelector(".binary-button").onclick = (event) => {
+    event.preventDefault();
+    BINARY_RESULT.textContent = makeBinary(BINARY_INPUT.value);
+}
 
-document.querySelector(".degree-button").onclick = () => DEGREE_RESULT.textContent = powOfSum(parseInt(DEGREE_INPUT.value));
+document.querySelector(".degree-button").onclick = (event) => {
+    event.preventDefault();
+    DEGREE_RESULT.textContent = powOfSum(parseInt(DEGREE_INPUT.value));
+}
 
-document.querySelector(".eleminate-button").onclick = () => ELEMINATE_RESULT.textContent = `Победил игрок под номером 
+document.querySelector(".eleminate-button").onclick = (event) => {
+    event.preventDefault();
+    ELEMINATE_RESULT.textContent = `Победил игрок под номером 
     ${eleminate(parseInt(ELEMINATE_INPUT_N.value), parseInt(ELEMINATE_INPUT_M.value))}`;
+}
