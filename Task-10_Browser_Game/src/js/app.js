a755a2a2f35cfe81c1e92f0c35e1643607ea1e6e
Yana <belozub.yanochka@mail.ru>
2022-01-25 11:03:18 +0400

    added background and foreground

--- Task-10_Browser_Game/src/js/app.js
+++ Task-10_Browser_Game/src/js/app.js
@@ -7,3 +7,55 @@ let frames = 0;
 
 const sprite = new Image();
 sprite.src = "images/content/sprite.png";
+
+const background = {
+    sX: 0,
+    sY: 0,
+    w: 275, 
+    h: 226,
+    x: 0, 
+    y: canvas.height - 226,
+
+    draw: function() {
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+
+    }
+
+}
+
+const foreground = {
+    sX: 276,
+    sY: 0,
+    w: 224, 
+    h: 112,
+    x: 0, 
+    y: canvas.height - 112,
+
+    draw: function() {
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        context.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+function draw() {
+    context.fillStyle = "#70c5ce";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    const body = document.getElementById("body");
+    background.draw();
+    foreground.draw();
+}
+
+function update() {
+
+}
+
+function loop() {
+    update();
+    draw();
+    frames++;
+
+    requestAnimationFrame(loop);
+}
+loop();
